94e73bc85fa41de66ef18785d7dfb2bc4674babe
phillip janowski <philajan@gmail.com>
2017-11-18 19:42:58 -0600

    first attempt ui

--- testdb.py
+++ testdb.py
@@ -10,21 +10,29 @@ db = MySQLdb.connect(host="localhost",    # your host, usually localhost
 cur = db.cursor()
 repeat = 'y'
 
+cur.execute("SHOW TABLES")
+
+
+tables = cur.fetchall()
 while repeat == 'y':
     #Customer table
     custname = raw_input('Cust Name:')
-    cust_num = raw_input('Cust Num:')
-    cust_paid = raw_input('Paid $:')
+    cust_num = input('Cust Num:')
+    cust_paid = input('Paid $:')
 
-    cur.execute("""
-                Insert into Customer
+    cur.execute(
+                """Insert into Customer
                 (Phone_num, Name, Amount_Paid) 
-                Values"""(cust_num ","
-                custname ","
-                cust_paid")")
+                Values(%s,%s,%s)""",
+                [cust_num, custname, cust_paid])
+    cur.execute("select * from Customer")
+    for row in cur:
+        print row
+    repeat = raw_input('Anotha one?')
+    
 
 # print all the first cell of all the rows
-for row in cur.fetchall():
-    print (row[2])
+#for row in cur.fetchall():
+ #   print (row[2])
 
 db.close()
